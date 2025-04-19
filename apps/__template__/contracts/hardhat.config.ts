8fba5bcf421afa062bade15ecbf332cb31f3df75
wangshouren <wangshouren@wangshourendeMacBook-Air.local>
2025-04-19 09:26:00 +0800

    chore: update template contracts

--- apps/__template__/contracts/hardhat.config.ts
+++ apps/__template__/contracts/hardhat.config.ts
@@ -1,28 +1,30 @@
 import { vars, type HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import { type NetworksUserConfig } from "hardhat/types";
-
-const hasSepoliaPrivateKey = vars.has("SEPOLIA_TEST_PRIVATE_KEY");
-
-const networks: NetworksUserConfig = {
-  ganache: {
-    url: "http://localhost:7545",
-    accounts: [
-      "0x341afa1e065408123210c5d54d1c8813f0db99fbc172f9fc388b60040f627f5b",
-    ],
-  },
-};
-
-if (hasSepoliaPrivateKey) {
-  networks.sepolia = {
-    url: "https://eth-sepolia.g.alchemy.com/v2/ktIe8-3T28q2B3tEnK2VhPr2izkBsRA9",
-    accounts: [vars.get("SEPOLIA_TEST_PRIVATE_KEY")],
-  };
-}
+import "@nomicfoundation/hardhat-verify";
+import { set } from "lodash";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
-  networks,
+  networks: {
+    ganache: {
+      url: "http://localhost:7545",
+      accounts: [
+        "0x341afa1e065408123210c5d54d1c8813f0db99fbc172f9fc388b60040f627f5b",
+      ],
+    },
+  },
 };
+
+if (vars.has("SEPOLIA_TEST_PRIVATE_KEY") && vars.has("SEPOLIA_TEST_RPC_URL")) {
+  set(config, "networks.sepolia", {
+    url: vars.get("SEPOLIA_TEST_RPC_URL"),
+    accounts: [vars.get("SEPOLIA_TEST_PRIVATE_KEY")],
+  });
+}
+
+if (vars.has("ETHERSCAN_API_KEY")) {
+  set(config, "etherscan.apiKey", vars.get("ETHERSCAN_API_KEY"));
+  set(config, "sourcify.enabled", true);
+}
 
 export default config;
--- apps/__template__/contracts/scripts/generate-index.ts
+++ apps/__template__/contracts/scripts/generate-index.ts
@@ -1,52 +1,62 @@
 import { writeFileSync } from "fs";
 import { artifacts } from "hardhat";
 import { format } from "prettier";
-import { getDeployedChainIdToAddresses } from "./utils";
+import { getDeployedChainIdToAddresses, importEsm } from "./utils";
 
 async function generateIndex() {
+  const { constantCase } = (await importEsm(
+    "change-case",
+    // eslint-disable-next-line @typescript-eslint/consistent-type-imports
+  )) as typeof import("change-case");
   const chainIdToAddresses = await getDeployedChainIdToAddresses();
 
-  const tokenAbi = (await artifacts.readArtifact("Token")).abi;
-  const exchangeAbi = (await artifacts.readArtifact("Exchange")).abi;
-  const tokenEvents = tokenAbi
-    .filter((abi) => abi.type === "event")
-    .reduce((acc, abi) => {
-      acc[abi.name] = abi;
-      return acc;
-    }, {});
+  const contractNames = Object.keys(
+    chainIdToAddresses[Object.keys(chainIdToAddresses)[0]],
+  );
 
-  const exchangeEvents = exchangeAbi
-    .filter((abi) => abi.type === "event")
-    .reduce((acc, abi) => {
-      acc[abi.name] = abi;
-      return acc;
-    }, {});
+  const contracts = await Promise.all(
+    contractNames.map(async (name) => {
+      const abi = (await artifacts.readArtifact(name)).abi;
+      const events = abi
+        .filter((abi) => abi.type === "event")
+        .reduce((acc, abi) => {
+          acc[abi.name] = abi;
+          return acc;
+        }, {});
+      return {
+        name,
+        abi,
+        events,
+      };
+    }),
+  );
 
   const code = `
   // The contract config for Frontend usage.
   // Automatically generated.
 
   export * from "./typechain-types";
-  
-  export const TOKEN_ABI = ${JSON.stringify(tokenAbi, null, 2)} as const;
-  export const EXCHANGE_ABI = ${JSON.stringify(exchangeAbi, null, 2)} as const;
 
-  export const TOKEN_EVENTS = ${JSON.stringify(tokenEvents, null, 2)} as const;
-  export const EXCHANGE_EVENTS = ${JSON.stringify(exchangeEvents, null, 2)} as const;
+  ${contracts.map((contract) => {
+    return `
+  export const ${constantCase(contract.name)}_ABI = ${JSON.stringify(contract.abi, null, 2)} as const;
+  export const ${constantCase(contract.name)}_EVENTS = ${JSON.stringify(contract.events, null, 2)} as const;
+  `;
+  })}
 
   export const CHAIN_ID_TO_CONTRACT_CONFIG = {
     ${Object.entries(chainIdToAddresses)
       .map(([chainId, addresses]) => {
         return `
       ${chainId}: {
-        token: {
-          address: "${addresses.Token}",
-          abi: TOKEN_ABI,
-        },
-        exchange: {
-          address: "${addresses.Exchange}",
-          abi: EXCHANGE_ABI,
-        },
+        ${contracts.map((contract) => {
+          return `
+          ${contract.name}: {
+            address: "${addresses[contract.name]}",
+            abi: ${constantCase(contract.name)}_ABI,
+          },
+          `;
+        })}
       },
       `;
       })
