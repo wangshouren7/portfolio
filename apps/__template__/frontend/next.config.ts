81040ee0c63ee5cde1ab67347418b826bb615f62
wangshouren <wangshouren@wangshourendeMacBook-Air.local>
2025-04-19 11:10:01 +0800

    ci: add nft-topia

--- apps/__template__/frontend/next.config.ts
+++ apps/__template__/frontend/next.config.ts
@@ -1,5 +1,16 @@
 import nextConfig from "@pfl-wsr/configs/next";
+import createNextIntlPlugin from "next-intl/plugin";
+
 import { merge } from "lodash-es";
 import { type NextConfig } from "next";
 
-export default merge(nextConfig, {} satisfies NextConfig);
+const withNextIntl = createNextIntlPlugin({
+  experimental: {
+    createMessagesDeclaration: [
+      "./src/i18n/messages/en.json",
+      "./src/i18n/messages/zh.json",
+    ],
+  },
+});
+
+export default withNextIntl(merge(nextConfig, {} satisfies NextConfig));
